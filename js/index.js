855408dd1165cd44692b97df94075e31295a652c
Piyush2504 <81811435+Piyush2504@users.noreply.github.com>
2022-03-08 22:33:47 +0530

    addded more sections of movies and web series

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@
 //     "url": `https://api.themoviedb.org/3/discover/movie?api_key=db5b8bfc146e2c55ab2417c30811f11f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`,
 //     "method": "GET",
 // }
+
+//For Movies
 $(document).ready(function() {
     $.ajax({
     url: "https://api.themoviedb.org/3/discover/movie?api_key=db5b8bfc146e2c55ab2417c30811f11f&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=1&with_watch_monetization_types=flatrate"
@@ -13,6 +15,7 @@ $(document).ready(function() {
     console.log(data);
     var cardDiv = document.getElementById("top_movies");
     cardDiv.innerHTML = ""
+    
     for (var i = 0; i < data.length; i++) {
         try {
             movie_name = data[i].original_title.slice(0,10);
@@ -25,11 +28,11 @@ $(document).ready(function() {
             // card.classList.add("col-xs-2");
             // card.classList.add("col-md-2");
             // card.classList.add("col-sm-2");
-            card.innerHTML = `
-                                    <img src="http://image.tmdb.org/t/p/original${photos}" class="card-img-top" alt="...">
+            card.innerHTML = `      
+                                    <img src="http://image.tmdb.org/t/p/original${photos}" class="card-img-top" alt="..." style="color:white; box-shadow: 2px 2px 2px 2px rgba(255, 255, 255, 0.1);">
                                     <div class="card-body">
-                                    <h6 class="card-title">${movie_name}</h6>
-                                    <p class="card-text">Rating: ${rating}</p>
+                                    <h6 class="card-title" style="color:white;">${movie_name}</h6>
+                                    <p class="card-text" style="color:white;">Rating: ${rating}</p>
                                     </div>
                                 `
 
@@ -37,5 +40,77 @@ $(document).ready(function() {
         } catch (e) { console.log(e) }
     }
     });
-    });
+});
+
+//For Web Series
+// $(document).ready(function() {
+//     $.ajax({
+//     url: "https://api.themoviedb.org/3/tv/popular?api_key=db5b8bfc146e2c55ab2417c30811f11f&language=en-US&page=1"
+//     }).then(function(data) {
+//     data=data.results;
+//     console.log(data);
+//     var cardDiv = document.getElementById("top_webSeries");
+//     cardDiv.innerHTML = ""
     
+//     for (var i = 0; i < data.length; i++) {
+//         try {
+//             web = data[i].name;
+//             rating = data[i].vote_average;
+//             photos = data[i].poster_path;
+
+//             var card = document.createElement('div');
+//             card.classList.add("card1");
+//             card.classList.add("col-2");
+//             // card.classList.add("col-xs-2");
+//             // card.classList.add("col-md-2");
+//             // card.classList.add("col-sm-2");
+//             card.innerHTML = `      
+//                                     <img src="http://image.tmdb.org/t/p/original${photos}" class="card-img-top" alt="..." style="color:white; box-shadow: 2px 2px 2px 2px rgba(255, 255, 255, 0.1);">
+//                                     <div class="card-body">
+//                                     <h6 class="card-title" style="color:white;">${web}</h6>
+//                                     <p class="card-text" style="color:white;">Rating: ${rating}</p>
+//                                     </div>
+//                                 `
+
+//             cardDiv.appendChild(card);
+//         } catch (e) { console.log(e) }
+//     }
+//     });
+// });
+
+// Language
+$(document).ready(function() {
+    $.ajax({
+    url: "https://api.themoviedb.org/3/discover/movie?api_key=db5b8bfc146e2c55ab2417c30811f11f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_original_language=hi&with_watch_monetization_types=flatrate"
+    }).then(function(data) {
+    data=data.results;
+    console.log(data);
+    var cardDiv = document.getElementById("top_webSeries");
+    cardDiv.innerHTML = ""
+    
+    for (var i = 0; i < data.length; i++) {
+        try {
+            web = data[i].original_title;
+            rating = data[i].vote_average;
+            photos = data[i].poster_path;
+
+            var card = document.createElement('div');
+            card.classList.add("card1");
+            card.classList.add("col-2");
+            // card.classList.add("col-xs-2");
+            // card.classList.add("col-md-2");
+            // card.classList.add("col-sm-2");
+            card.innerHTML = `      
+                                    <img src="http://image.tmdb.org/t/p/original${photos}" class="card-img-top" alt="..." style="color:white; box-shadow: 2px 2px 2px 2px rgba(255, 255, 255, 0.1);">
+                                    <div class="card-body">
+                                    <h6 class="card-title" style="color:white;">${web}</h6>
+                                    <p class="card-text" style="color:white;">Rating: ${rating}</p>
+                                    </div>
+                                `
+
+            cardDiv.appendChild(card);
+            
+        } catch (e) { console.log(e) }
+    }
+    });
+});
